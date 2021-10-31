8efd0ef2e4ccca784267265754c4547453735cb6
William Jones <37749088+wdj82@users.noreply.github.com>
2021-10-30 19:16:38 -0500

    url problems

--- util/graphCMSClient.js
+++ util/graphCMSClient.js
@@ -1,15 +1,21 @@
 import { gql, GraphQLClient } from 'graphql-request';
 
-const graphCMSClient = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT);
-const graphCMSMutationClient = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, {
-    headers: {
-        Authorization: `Bearer ${process.env.GRAPHCMS_MUTATION_TOKEN}`,
+const graphCMSClient = new GraphQLClient('https://api-us-east-1.graphcms.com/v2/ckv9uvsib102201z25vxo6jsw/master');
+const graphCMSMutationClient = new GraphQLClient(
+    'https://api-us-east-1.graphcms.com/v2/ckv9uvsib102201z25vxo6jsw/master',
+    {
+        headers: {
+            Authorization: `Bearer ${process.env.GRAPHCMS_MUTATION_TOKEN}`,
+        },
     },
-});
-const graphCMSOrdersClient = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, {
-    headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_ORDERS_TOKEN}`,
+);
+const graphCMSOrdersClient = new GraphQLClient(
+    'https://api-us-east-1.graphcms.com/v2/ckv9uvsib102201z25vxo6jsw/master',
+    {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_ORDERS_TOKEN}`,
+        },
     },
-});
+);
 
 export { gql, graphCMSClient, graphCMSMutationClient, graphCMSOrdersClient };
