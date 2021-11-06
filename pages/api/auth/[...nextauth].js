55637c4cadfbed501eda16cc1fe0ad4764b5b845
William Jones <37749088+wdj82@users.noreply.github.com>
2021-11-06 12:56:32 -0500

    passwordless login setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_HOST,
+                port: process.env.EMAIL_PORT,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.TOKEN_SECRET,
+});
