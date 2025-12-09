4dfdbce3ada9e8bbca39ab9aca9acc1ea71d8424
William Jones <37749088+wdj82@users.noreply.github.com>
2025-12-08 18:45:13 -0600

    update image domains

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        // domains: ['res.cloudinary.com'],
-        domains: ['media.graphassets.com'],
+        domains: ['media.graphassets.com', 'us-east-1.graphassets.com'],
     },
     generateBuildId: () => 'build',
     webpack: (config, { isServer }) => {
